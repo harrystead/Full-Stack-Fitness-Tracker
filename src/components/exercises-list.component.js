a10c4a74f23c7b691a00a5bbe057f3ea374322ce
Harry Stead <harrystead9@gmail.com>
2021-02-15 12:32:31 +0000

    create user component

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 export default class ExercisesList extends Component {
-    // constructor(){
-    //     super()
-    // }
+    constructor(props){
+        super(props);
+     
+    }
     render(){
         return(
             <div>
