3059eada723ebd037b1c81a6b711afdc0f59000e
Harry Stead <harrystead9@gmail.com>
2021-02-15 13:52:56 +0000

    user add is working

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://harry123:harryjack1214@cluster0.7trlh.mongodb.net/Fitness?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
