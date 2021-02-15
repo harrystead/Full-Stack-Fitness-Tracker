953faaabb966a7c790c05a01dc9cc71433b46889
Harry Stead <harrystead9@gmail.com>
2021-02-15 11:34:53 +0000

    frontend set up

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+    // constructor(){
+    //     super()
+    // }
+    render(){
+        return(
+            <div>
+                <h1>whatsgood</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+    // constructor(){
+    //     super()
+    // }
+    render(){
+        return(
+            <div>
+                <h1>howjswosj</h1>
+            </div>
+        )
+    }
+}
